74a886942ab00809097a6328de94bf7d90f290ab
NithyaVadivel <nithya.vadivel@a-cti.com>
2019-10-21 20:51:46 +0530

    Added configurations

---
+++ test.js
@@ -0,0 +1,14 @@
+const assert = require('assert');
+
+
+
+describe('Fetch the URL', function(){
+   
+
+    it('login url ', function(){
+          //maximise the window
+          browser.windowHandleFullscreen();
+          browser.url('https://staging.setmore.com/');
+    });
+
+});
